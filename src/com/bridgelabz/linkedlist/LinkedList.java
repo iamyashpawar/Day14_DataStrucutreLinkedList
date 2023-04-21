207acca4eb1a523599aa3a0515d8d4f1579c8d89
Yash <imyashpawar@gmail.com>
2023-04-21 14:18:41 +0530

    Added: First Commit

---
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -0,0 +1,34 @@
+package com.bridgelabz.linkedlist;
+
+public class LinkedList<T> {
+
+    Node <T> head;
+    Node <T> tail;
+    public void push(T data) {
+
+        Node<T> newNode1 = new Node<>(data);
+       if(head == null)
+       {
+          head = newNode1;
+          tail = newNode1;
+       }else {
+          newNode1.next = head;
+          head = newNode1;
+       }
+
+    }
+
+
+    public void display() {
+        Node<T> temp = head;
+     while (temp != null)
+     {
+         System.out.print(temp.data + " -> ");
+         temp = temp.next;
+
+
+     }
+        System.out.println();
+
+    }
+}
---
+++ src/com/bridgelabz/linkedlist/Node.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.linkedlist;
+
+public class Node<T>{
+
+    T data;
+    Node<T> next; // null
+
+    public Node(T data) {
+        this.data = data;
+    }
+
+
+
+
+
+}
